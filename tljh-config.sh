af6666b9c0742ebcc48b45a7939ad783fa0b6a4f
parkcoj <parkcoj@nih.gov>
2022-10-20 14:33:48 +0000

    add complete example

--- tljh-config.sh
+++ tljh-config.sh
@@ -13,8 +13,8 @@ sudo -E python -m bash_kernel.install
 sudo mkdir -p /srv/{data,programs}/
 cd /srv/programs/
 
-sudo curl -o magicblast.tar.gz https://ftp.ncbi.nlm.nih.gov/blast/executables/magicblast/LATEST/ncbi-magicblast-1.6.0-x64-linux.tar.gz
-sudo tar -xvzf magicblast.tar.gz && sudo chmod -R 755 ncbi-magicblast-1.6.0/
+sudo curl -o magicblast.tar.gz https://ftp.ncbi.nlm.nih.gov/blast/executables/magicblast/LATEST/ncbi-magicblast-1.7.0-x64-linux.tar.gz
+sudo tar -xvzf magicblast.tar.gz && sudo chmod -R 755 ncbi-magicblast-1.7.0/
 
 sudo curl -o blast.tar.gz https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.13.0+-x64-linux.tar.gz
 sudo tar -xvzf blast.tar.gz && sudo chmod -R 755 ncbi-blast-2.13.0+
@@ -37,7 +37,7 @@ sudo mv 16S* 16S_ribosomal_RNA/
 
 # Create symbolic links for programs and data
 cd /etc/skel
-sudo ln -s /srv/programs/ncbi-magicblast-1.6.0/bin/magicblast .
+sudo ln -s /srv/programs/ncbi-magicblast-1.7.0/bin/magicblast .
 sudo ln -s /srv/programs/ncbi-blast-2.13.0+/bin/blastdbcmd .
 sudo ln -s /srv/programs/bin/bin/ktImportBLAST .
 sudo ln -s /srv/programs/bin/bin/ktImportTaxonomy .
